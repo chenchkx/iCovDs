53764b6c4419dc2a266da1076254d4d396b9ca1b
Kai-Xuan Chen <36260120+Kai-Xuan@users.noreply.github.com>
2019-09-03 22:51:30 +0800

    del

--- tool_Kit/get_Dis_Matrix_Path_YTC_Fold.m
+++
@@ -1,5 +0,0 @@
-%% Author: Kai-Xuan Chen 
-
-function dis_Matrix_Path = get_Dis_Matrix_Path_YTC_Fold(option,fold_th)
-    dis_Matrix_Path = [option.mat_Path,'\','disMatrixFold',num2str(fold_th),'_',option.out_File_Name];
-end
